8071a511e5c36dfe211e193d354a68d23c6e60de
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-08-16 12:05:32 -0300

    Primer constructor
    Primer constructor con datos por defecto

--- src/EJERCICIO1/Empleado.java
+++ src/EJERCICIO1/Empleado.java
@@ -4,4 +4,15 @@ public class Empleado {
 	private int id;
 	private String nombre;
 	private int edad;
+	
+	static int contador = 999;
+	
+	//Constructores
+	
+		public Empleado() {
+			contador++;
+			id = contador;
+			nombre = "sin nombre";
+			edad = 99;		
+		}
 }
