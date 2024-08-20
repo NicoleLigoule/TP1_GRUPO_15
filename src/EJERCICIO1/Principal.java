7ea0cd78244efdcfcc650b3c3e553073efe9fdbf
NicoleLigoule <nicole.ligoule@alumnos.frgp.utn.edu.ar>
2024-08-19 21:51:43 -0300

    Agregue el llamado al metodo devuelveProximoId() y un registro al array de Empleado

--- src/EJERCICIO1/Principal.java
+++ src/EJERCICIO1/Principal.java
@@ -7,8 +7,11 @@ public class Principal {
 		Empleado[] vEmp = new Empleado[5];
 		vEmp[0] = new Empleado("Jorge",26);
 		vEmp[1] = new Empleado();
+		vEmp[2] = new Empleado("Nicole",23);
+
 		
-		
+		// llamado al metodo
+		System.out.print("El próximo ID será el " + Empleado.devuelveProximoId());	
 	}
 
 }
